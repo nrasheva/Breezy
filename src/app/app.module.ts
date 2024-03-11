498dfd540dae13fc0a2b932e928f023ab2b054ae
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-11 22:36:01 +0200

    Code refining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
