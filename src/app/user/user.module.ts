702d72fb52fac715ee5424eef371fd53c53b959a
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-11 22:35:30 +0200

    Update user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-
+import { UserRoutingModule } from './user-routing-module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
