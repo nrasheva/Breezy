3e0247e84994608715061141c81c24513d617889
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-17 20:46:21 +0200

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LocationSearchComponent } from './location-search/location-search.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { QualityInfoMessageComponent } from './quality-info-message/quality-info-message.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LocationSearchComponent, QualityInfoMessageComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [LocationSearchComponent, QualityInfoMessageComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
