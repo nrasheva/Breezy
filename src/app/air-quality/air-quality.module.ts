8c3d8274793ce1bd0c8420359abc46139a48adc1
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-17 23:20:37 +0200

    Update air-quality module

--- src/app/air-quality/air-quality.module.ts
+++ src/app/air-quality/air-quality.module.ts
@@ -4,10 +4,11 @@ import { IndicatorsComponent } from './indicators/indicators.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MapComponent } from './map/map.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [MainViewComponent, MapComponent, IndicatorsComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, SharedModule],
   exports: [MainViewComponent],
 })
 export class AirQualityModule {}
