40da4d68b82b57981ab183bd6ea74ca5c4c31083
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-18 22:32:46 +0200

    Update location-search components

--- src/app/shared/location-search/location-search.component.ts
+++ src/app/shared/location-search/location-search.component.ts
@@ -7,5 +7,15 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./location-search.component.css'],
 })
 export class LocationSearchComponent {
+  location = '';
+
   faArrowRight = faArrowRight;
+
+  constructor() {}
+
+  onSubmit() {
+    // Here, you can make the request to the server using the value from `this.location`
+    console.log('Location submitted:', this.location);
+    // Add your request logic here
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LocationSearchComponent } from './location-search/location-search.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { QualityInfoMessageComponent } from './quality-info-message/quality-info-message.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LocationSearchComponent, QualityInfoMessageComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
   exports: [LocationSearchComponent, QualityInfoMessageComponent]
 })
 export class SharedModule {}
