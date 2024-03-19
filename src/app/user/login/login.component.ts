e05ceab6c92bfdb10dabd8561b28293f742a6b81
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-19 22:36:54 +0200

    Update Login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  user: User = {
+    name: '',
+    password: ''
+  };
 
+  // constructor() {}
+
+  login(formData: User) {
+    console.log('Form Data:', formData);
+    // Here, you'd implement your actual login logic, such as calling an authentication service
+  }
 }
