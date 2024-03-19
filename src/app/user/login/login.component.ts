73bc004727a3f3e336c4da3ad448a1c7bf0adced
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-20 00:07:27 +0200

    Update login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/types/User';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,16 @@ import { User } from 'src/app/types/User';
 export class LoginComponent {
   user: User = {
     name: '',
-    password: ''
+    password: '',
   };
 
   // constructor() {}
 
-  login(formData: User) {
+  login(formData: NgForm) {
+    if (formData.invalid) {
+      console.log('Form is invalid');
+      return;
+    }
     console.log('Form Data:', formData);
     // Here, you'd implement your actual login logic, such as calling an authentication service
   }
