882115aee0cfe4d83102cdc65c280b109669a629
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-20 00:07:40 +0200

    Update register component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/types/User';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  user: User = {
+    name: '',
+    password: '',
+  };
 
+  // constructor() {}
+
+  register(formData: NgForm) {
+    if (formData.invalid) {
+      console.log('Form is invalid');
+      return;
+    }
+    console.log('Form Data:', formData);
+    // Here, you'd implement your actual login logic, such as calling an authentication service
+  }
 }
