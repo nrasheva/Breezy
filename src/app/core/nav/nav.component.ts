a5009101c9ae27f3e2a0d86b3710c96956481153
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-25 23:08:56 +0200

    Update nav component ts

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserService } from 'src/app/user/user.service';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +9,7 @@ import { UserService } from 'src/app/user/user.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit, OnDestroy {
+  faRightFromBracket = faRightFromBracket;
   showMenu = window.innerWidth > 768;
   isLoggedIn = false;
   private authSubscription!: Subscription;
@@ -42,6 +44,10 @@ export class NavComponent implements OnInit, OnDestroy {
 
   private checkScreenWidth() {
     this.showMenu = window.innerWidth > 768;
+  }
+
+  logout() {
+    this.userService.logout();
   }
 
   ngOnDestroy(): void {
