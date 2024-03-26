53f53c4e0b4c70719db2146404d54906dc3cbc33
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-26 21:27:20 +0200

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,19 @@ import { LocationSearchComponent } from './location-search/location-search.compo
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { QualityInfoMessageComponent } from './quality-info-message/quality-info-message.component';
 import { FormsModule } from '@angular/forms';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [LocationSearchComponent, QualityInfoMessageComponent],
+  declarations: [
+    LocationSearchComponent,
+    QualityInfoMessageComponent,
+    LoaderComponent,
+  ],
   imports: [CommonModule, FontAwesomeModule, FormsModule],
-  exports: [LocationSearchComponent, QualityInfoMessageComponent]
+  exports: [
+    LocationSearchComponent,
+    QualityInfoMessageComponent,
+    LoaderComponent,
+  ],
 })
 export class SharedModule {}
