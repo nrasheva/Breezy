11d2770751cd7849aca7610b31956b6fc0efbbbf
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-30 14:21:26 +0200

    Update air-quality service

--- src/app/shared/services/air-quality-service.service.ts
+++ src/app/shared/services/air-quality-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { LocationCoordinatesService } from './location-coordinates.service';
 import { AirQualityData } from 'src/app/types/AirQualityData';
 
@@ -9,24 +9,30 @@ import { AirQualityData } from 'src/app/types/AirQualityData';
   providedIn: 'root',
 })
 export class AirQualityServiceService {
+  private airQualityDataSource = new BehaviorSubject<AirQualityData | null>(
+    null
+  );
+  airQualityData$ = this.airQualityDataSource.asObservable();
+
   constructor(
     private http: HttpClient,
     private LocationCoordinatesService: LocationCoordinatesService
   ) {}
 
   fetchAirQuality(lat: number, lon: number): Observable<AirQualityData> {
-    const baseURL = '/api/getAirQuality'; // Adjust if your base URL is different
+    const baseURL = '/api/getAirQuality'; 
     const params = { params: { lat, lng: lon } };
-    return this.http.get<{ airQuality: { current: AirQualityData } }>(baseURL, params).pipe(
-      map(response => response.airQuality.current)
-    );
+    return this.http
+      .get<{ airQuality: { current: AirQualityData } }>(baseURL, params)
+      .pipe(map(response => response.airQuality.current));
   }
 
   fetchAirQualityForCurrentLocation(): Observable<AirQualityData> {
     return this.LocationCoordinatesService.currentLocationData.pipe(
       switchMap(({ latitude, longitude }) =>
         this.fetchAirQuality(latitude, longitude)
-      )
+      ),
+      tap((airQualityData: AirQualityData) => this.airQualityDataSource.next(airQualityData))
     );
   }
 }
