4484654743bf19fbd3ebc00b2396cfb7a6d0c0ca
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-30 14:20:34 +0200

    Update user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,9 +5,10 @@ import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing-module';
 import { FormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule, UserRoutingModule, FormsModule],
+  imports: [CommonModule, UserRoutingModule, FormsModule, FontAwesomeModule],
 })
 export class UserModule {}
