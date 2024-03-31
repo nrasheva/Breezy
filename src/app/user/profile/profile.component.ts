f31014ac587ce961cb3ed523b91fda18fbfe8a23
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-31 20:19:59 +0300

    Update profile component ts

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -10,6 +10,8 @@ import { Location } from 'src/app/types/Location';
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   currentLocation: Location | null = null;
+  isEditing = false;
+  editableLocation = '';
   private locationSubscription: Subscription | undefined;
 
   constructor(private userService: UserService) {}
@@ -20,6 +22,44 @@ export class ProfileComponent implements OnInit, OnDestroy {
       .subscribe(location => {
         this.currentLocation = location;
       });
+  }
+
+  startEditing(): void {
+    this.isEditing = true;
+  }
+
+  stopEditing(): void {
+    this.isEditing = false;
+  }
+
+  updateLocation(): void {
+    const currentLocation = this.currentLocation;
+
+    if (currentLocation && this.editableLocation.trim()) {
+      this.userService
+        .editLocation(currentLocation._id, this.editableLocation)
+        .subscribe(
+          () => {
+            console.log('Location updated successfully');
+            if (currentLocation) {
+              // Update the local state
+              currentLocation.location = this.editableLocation;
+
+              localStorage.setItem(
+                'currentLocation',
+                JSON.stringify(currentLocation)
+              );
+
+              this.userService.setCurrentLocation(currentLocation);
+            }
+
+            this.isEditing = false; // Exit editing mode
+          },
+          error => {
+            console.error('Error updating location:', error);
+          }
+        );
+    }
   }
 
   ngOnDestroy(): void {
