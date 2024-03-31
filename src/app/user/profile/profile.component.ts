e73bcaf3a452362f877b9ef3f00d62db1ba2673a
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-31 10:38:52 +0300

    Update profile component ts

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,12 +1,30 @@
-
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from '../user.service';
+import { Location } from 'src/app/types/Location';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  currentLocation: Location | null = null;
+  private locationSubscription: Subscription | undefined;
 
-  constructor() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.locationSubscription = this.userService
+      .getCurrentLocation()
+      .subscribe(location => {
+        this.currentLocation = location;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.locationSubscription) {
+      this.locationSubscription.unsubscribe();
+    }
+  }
 }
