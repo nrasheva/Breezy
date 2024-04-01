31b8653bf0c5ecfb9c3440a1767bae854bb58c5e
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-04-01 15:11:39 +0300

    Update app interceptor

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -5,27 +5,58 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
-
-const { apiUrl } = environment;
+import { Observable, catchError, throwError } from 'rxjs';
+import { ErrorService } from './core/error/error.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
-  API = '/api';
-  // constructor() {}
+  constructor(
+    private errorService: ErrorService,
+    private router: Router
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     console.log(request);
-    if (request.url.startsWith(this.API)) {
-      request = request.clone({
-        url: request.url.replace(this.API, apiUrl),
-        withCredentials: true,
+
+    const authToken = localStorage.getItem('authToken');
+
+    if (authToken) {
+      if (this.isTokenExpired(authToken)) {
+        localStorage.removeItem('authToken');
+        this.router.navigate(['/login']);
+        return throwError(() => new Error('Token has expired'));
+      }
+
+      const authReq = request.clone({
+        headers: request.headers.set('Authorization', `Bearer ${authToken}`),
       });
+      return next.handle(authReq);
+    } else {
+      // this.router.navigate(['/home']);
     }
-    return next.handle(request);
+
+    // Handling errors globally
+    return next.handle(request).pipe(
+      catchError(err => {
+        if (err.status === 403) {
+          console.log('Not authenticated');
+          this.router.navigate(['/login']);
+        } else {
+          this.errorService.setError(err);
+          this.router.navigate(['/error']);
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+
+  private isTokenExpired(token: string): boolean {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const now = Date.now() / 1000;
+    return payload.exp < now;
   }
 }
