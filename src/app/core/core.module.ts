ed0016f96a7a9d925d6710ce2c8ad71eb524dd71
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-04-02 22:23:46 +0300

    Update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavComponent],
+  declarations: [NavComponent, ErrorComponent, FooterComponent],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
-  exports: [NavComponent],
+  exports: [NavComponent, FooterComponent],
 })
 export class CoreModule {}
