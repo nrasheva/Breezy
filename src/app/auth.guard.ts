4b02e0d7333a501a1fea633916dcbb41c93c490c
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-04-02 23:06:03 +0300

    Implement auth guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const authToken = localStorage.getItem('authToken');
+    if (authToken) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
