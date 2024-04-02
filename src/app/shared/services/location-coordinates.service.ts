e570a6eb18f54d7688f5df1a321fbb177bea4fd3
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-04-02 09:36:51 +0300

    Update location-coordinates

--- src/app/shared/services/location-coordinates.service.ts
+++ src/app/shared/services/location-coordinates.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { GeoapifyResponse } from 'src/app/types/GeoapifyResponse';
 
 export interface LocationData {
   latitude: number;
@@ -14,12 +16,29 @@ export class LocationCoordinatesService {
     latitude: 0,
     longitude: 0,
   });
-
   currentLocationData = this.locationDataSource.asObservable();
 
-  // constructor() {}
+  constructor(private http: HttpClient) {}
 
   updateLocationData(locationData: LocationData) {
     this.locationDataSource.next(locationData);
+  }
+
+  fetchLocationCoordinates(location: string) {
+    const params = { location: location };
+    this.http
+      .get<GeoapifyResponse>('/api/getLocationCoordinates', { params })
+      .subscribe({
+        next: response => {
+          const coordinates =
+            response.locationCoordinates.features[0].geometry.coordinates;
+          const longitude = coordinates[0];
+          const latitude = coordinates[1];
+          this.updateLocationData({ latitude, longitude });
+          console.log(latitude, longitude);
+        },
+        error: error =>
+          console.error('Error fetching location coordinates:', error),
+      });
   }
 }
