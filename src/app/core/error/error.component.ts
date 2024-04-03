e3149e75d428835c7e74da569455dbe6091e12ff
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-04-03 21:38:11 +0300

    Update error component ts

--- src/app/core/error/error.component.ts
+++ src/app/core/error/error.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ErrorService } from './error.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-error',
@@ -9,17 +10,33 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class ErrorComponent implements OnInit {
   errorMessage = '';
+  isShown = true;
 
-  constructor(private errorService: ErrorService) {}
+  constructor(
+    private errorService: ErrorService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.errorService.apiError$.subscribe((err: HttpErrorResponse | null) => {
       if (err) {
         this.errorMessage =
-          err.error?.message || err.statusText || 'An unexpected error occurred';
+          err.error?.message ||
+          err.statusText ||
+          'An unexpected error occurred';
       } else {
         this.errorMessage = '';
+        this.isShown = false; 
       }
     });
+  }
+
+  hideError(): void {
+    this.isShown = false;
+  }
+
+  onOkClick(): void {
+    this.hideError();
+    this.location.back(); 
   }
 }
